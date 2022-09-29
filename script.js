c51e555b0d30611fc320f63ec87e9ed9f7649657
Rafaela <rafalasantosdarosa@yahoo.com>
2022-09-29 11:47:21 -0300

    Adicionado luz ambiente e ponto de luz

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ render.setSize(window.innerWidth, window.innerHeight);
 
 var canvas = render.domElement;
 document.body.appendChild(canvas);
+
+var geometriaCubo = new THREE.BoxGeometry(1, 1, 1);
+
+var materialCubo = new THREE.MeshLambertMaterial({color: 0xE933FF});
+
+var cubo = new THREE.Mesh(geometriaCubo, materialCubo);
+cena.add(cubo);
+
+var luzAmbiente = new THREE.AmbientLight(0x333333);
+cena.add(luzAmbiente);
+
+var luzPonto = new THREE.PointLight (0x888888);
+luzPonto.position.set(2, 2, 4);
+
+cena.add(luzPonto);
+camera.position.z = 5;
+
+var xi; 
+var yi;
+
+canvas.addEventListener("mousedown", function (e){
+    xi = e.offsetX;
+    yi = e.offsetY;
+}, false);
+
+canvas.addEventListener("mousemove", function (e){
+    if(e.buttons > 0){
+        camera.position.x = 8 * (xi - e.offsetX) / canvas.width;
+        camera.position.y = 8 * (e.offsetY - yi) / canvas.height;
+    }
+}, false)
+
+function desenhar(){
+    render.render(cena, camera);
+    requestAnimationFrame(desenhar);
+}
+
+requestAnimationFrame(desenhar);
